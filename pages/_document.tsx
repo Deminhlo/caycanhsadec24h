cc59842ac3673b981fb1eb1fa9c732b63460ef64
Deminhlo <95955187+Deminhlo@users.noreply.github.com>
2024-02-05 14:59:03 +0700

    add Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import data from 'data/data.json'
-
+import { Analytics } from '@vercel/analytics/react';
 export default function Document() {
   return (
     <Html lang="en" style={{ backgroundColor: data.background }} className="scroll-smooth">
@@ -8,10 +8,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W2DQW3PJ"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
+        <Analytics />
       </body>
     </Html>
   )
