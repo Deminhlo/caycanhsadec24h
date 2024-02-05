6418936bdd3c9bf3d42d4a4a6f4a28b9dabc1438
Deminhlo <95955187+Deminhlo@users.noreply.github.com>
2024-02-05 14:44:45 +0700

    update gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W2DQW3PJ"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
       </body>
     </Html>
   )
